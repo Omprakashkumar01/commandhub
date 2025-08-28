084edb278669ec537855b15832483914b33fa8f2
Om Prakash kumar <112559646+Omprakashkumar01@users.noreply.github.com>
2025-08-29 05:24:40 +0530

    Create command_utils.py

---
+++ utils/command_utils.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def run_command(command: str):
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        return result.stdout.strip(), result.stderr.strip()
+    except Exception as e:
+        return "", str(e)
